de3ae8addd9fd2486af20b66045f9c377f72d187
alicja.mruk <alicja.mruk99@gmail.com>
2022-11-13 11:38:23 +0100

    chore: add transactions screen

--- src/modules/signed-in/modules/Transactions/Transactions.tsx
+++ src/modules/signed-in/modules/Transactions/Transactions.tsx
@@ -1,64 +1,115 @@
 import React, { useMemo } from 'react';
 
-import _ from 'lodash';
-import { SectionList, Text } from 'native-base';
+
+import { Box, Circle, Divider, HStack, Pressable, SectionList, Text, VStack } from 'native-base';
 import { useTranslation } from 'react-i18next';
 
-import { ContentWrapper } from 'components';
+import { Constants } from 'config/constants';
 import { useGetExpenses } from 'hooks/api';
 import { Expense } from 'models';
+
+import { getIconByName } from '../Categories/components/CategoryItem';
+
+// TODO: fix types
+const groupByDate = (expenses: Expense[]) => {
+  const groups = expenses.reduce((result, expense) => {
+    const date = expense.creationDate.split('T')[0];
+    if (!result[date]) {
+      result[date] = [];
+    }
+    result[date].push(expense);
+    return result;
+  }, {});
+
+  return Object.keys(groups).map(date => {
+    return {
+      date,
+      data: groups[date] as Expense[],
+    };
+  });
+};
 
 export const Transactions = () => {
   const { t } = useTranslation();
   const { data } = useGetExpenses();
 
-  // const sectionData = useMemo(() => {
-  //   const grouped = data?.reduce((prev: Expense, curr: Expense) => {
-  //     const date = new Date(curr.creationDate);
-  //     const key = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+  const sectionData = useMemo(() => groupByDate(data ?? []), [data]);
 
-  //     if (!prev[key]) {
-  //       prev[key] = [curr];
-  //     } else {
-  //       prev[key].push(curr);
-  //     }
+  const renderItem = ({ item }: { item: Expense }) => {
+    const onPress = () => {};
 
-  //     return prev;
-  //   }, []);
-  // }, [data]);
+    return (
+      <Pressable onPress={onPress} mx="4">
+        {({ isPressed }) => (
+          <Box>
+            <HStack
+              space="4"
+              alignItems="center"
+              justifyContent="space-between"
+              bg={isPressed ? 'secondary.400:alpha.20' : 'transparent'}>
+              <HStack space="4" alignItems="center">
+                <Circle size="12" bg={item.category.color} mt="2">
+                  {getIconByName(item.category.iconName)}
+                </Circle>
 
-  // const DATA = [
-  //   {
-  //     title: 'Main dishes',
-  //     data: ['Pizza', 'Burger', 'Risotto'],
-  //   },
-  //   {
-  //     title: 'Sides',
-  //     data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
-  //   },
-  //   {
-  //     title: 'Drinks',
-  //     data: ['Water', 'Coke', 'Beer'],
-  //   },
-  //   {
-  //     title: 'Desserts',
-  //     data: ['Cheese Cake', 'Ice Cream'],
-  //   },
-  // ];
+                <VStack>
+                  <Text variant="body" color={item.category.color} noOfLines={1}>
+                    {item.category.categoryName}
+                  </Text>
+                  <Text variant="body" color={item.category.color} noOfLines={1}>
+                    {item.name}
+                  </Text>
+                </VStack>
+              </HStack>
 
-  // // TODO: specify
-  // const renderItem = ({ item }: { item: any }) => {};
+              <Text variant="body" color="red.500" noOfLines={1}>
+                - {item.value} {Constants.CURRENCY}
+              </Text>
+            </HStack>
+            <Divider bg="gray.200" mt="2" />
+          </Box>
+        )}
+      </Pressable>
+    );
+  };
+
+  const renderSectionHeader = ({ section: { date } }: { section: { date: string } }) => {
+    const sectionExpenses = sectionData.find(item => item.date === date)?.data;
+    const totalSectionExpense = sectionExpenses?.reduce((acc, expense) => acc + expense.value, 0);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const dateFormatted = new Date(date);
+
+    return (
+      <HStack justifyContent="space-between" alignItems="center" bg="gray.200" p="3">
+        <Text variant="bodyBold">{dateFormatted.toLocaleDateString('en-GB', options)}</Text>
+        <Text color="warning.200" variant="h3">
+          - {totalSectionExpense} {Constants.CURRENCY}
+        </Text>
+      </HStack>
+    );
+  };
 
   return (
-    <ContentWrapper>
-      <Text variant="h1">{t('signedIn.transactions.title')}</Text>
+    <VStack py="4" flex="1" safeArea bg="white">
+      <Text variant="h1" px="4">
+        {t('signedIn.transactions.title')}
+      </Text>
 
-      {/* <SectionList
-        sections={DATA}
-        keyExtractor={(item, index) => item + index}
-        renderItem={renderItem}
-        renderSectionHeader={({ section: { key } }) => <Text>{key}</Text>}
-      /> */}
-    </ContentWrapper>
+      {data && data.length > 1 ? (
+        <SectionList
+          contentContainerStyle={{ paddingVertical: 16 }}
+          stickySectionHeadersEnabled
+          showsVerticalScrollIndicator={false}
+          bounces={false}
+          sections={sectionData}
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+          renderSectionHeader={renderSectionHeader}
+        />
+      ) : (
+        // TODO: finish
+        <Text>No Data</Text>
+      )}
+    </VStack>
   );
-};;
+};
