e2a40f1024b65b6feebfb553437397de0f0115c7
alicja.mruk <alicja.mruk@treelineinteractive.com>
2022-11-13 11:55:56 +0100

    chore: display user profile data

--- src/modules/signed-in/modules/Settings/Settings.tsx
+++ src/modules/signed-in/modules/Settings/Settings.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-
-import { Button, Text } from 'native-base';
+import { Box, Button, Spinner, Text, VStack } from 'native-base';
 import { useTranslation } from 'react-i18next';
 
 import { ContentWrapper } from 'components';
 import { useAuthContext } from 'contexts';
+import { useGetProfile } from 'hooks/api';
+
+import { UserInfo } from './UserInfo';
 
 export const Settings = () => {
   const { t } = useTranslation();
   const { logout } = useAuthContext();
+
+  const { data, isLoading } = useGetProfile();
 
   const onLogoutPress = async () => {
     await logout();
@@ -17,7 +21,22 @@ export const Settings = () => {
 
   return (
     <ContentWrapper justifyContent="space-between">
-      <Text variant="h1">{t('signedIn.settings.title')}</Text>
+      <Box>
+        <Text variant="h1">{t('signedIn.settings.title')}</Text>
+        <Box mt="12">
+          {isLoading || !data ? (
+            <Spinner />
+          ) : (
+            <VStack space="2">
+              <UserInfo label={t('signedIn.settings.user.email')} value={data.email} />
+              <UserInfo label={t('signedIn.settings.user.firstName')} value={data.firstName} />
+              <UserInfo label={t('signedIn.settings.user.lastName')} value={data.lastName} />
+              <UserInfo label={t('signedIn.settings.user.age')} value={data.age.toString()} />
+            </VStack>
+          )}
+        </Box>
+      </Box>
+
       <Button onPress={onLogoutPress}>{t('signedIn.settings.logout')}</Button>
     </ContentWrapper>
   );
