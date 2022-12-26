8c648e8cf02f32388f7f87ec5a4e6389b150e42c
alicja.mruk <alicja.mruk99@gmail.com>
2022-12-26 11:50:38 +0100

    finish change of design update expense

--- src/modules/signed-in/modules/Transactions/components/TransactionListItem.tsx
+++ src/modules/signed-in/modules/Transactions/components/TransactionListItem.tsx
@@ -15,22 +15,22 @@ export const TransactionListItem = ({ item }: Props) => {
     <Box mx="4">
       <HStack space="4" alignItems="center" justifyContent="space-between" bg="transparent">
         <HStack space="4" alignItems="center">
-          <Circle size="12" bg={item.category.color} mt="2">
-            {getIconByName(item.category.iconName)}
+          <Circle size="12" bg={item?.category?.color} mt="2">
+            {getIconByName(item?.category?.iconName)}
           </Circle>
 
           <VStack>
-            <Text variant="body" color={item.category.color} noOfLines={1}>
-              {item.category.categoryName}
+            <Text variant="body" color={item?.category?.color} noOfLines={1}>
+              {item?.category?.categoryName}
             </Text>
-            <Text variant="body" color={item.category.color} noOfLines={1}>
-              {item.name}
+            <Text variant="body" color={item?.category?.color} noOfLines={1}>
+              {item?.name}
             </Text>
           </VStack>
         </HStack>
 
         <Text variant="body" color="red.500" noOfLines={1}>
-          - {item.value} {Constants.CURRENCY}
+          - {item?.value} {Constants.CURRENCY}
         </Text>
       </HStack>
       <Divider bg="gray.200" mt="2" />
